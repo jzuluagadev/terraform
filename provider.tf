f565db0f75e8f281e90d95e7ab14734cb1132e0e
Juan <jzuluagagomezbotero@gmail.com>
2025-01-07 18:47:45 -0500

    Crete Vpc con AK and Sk

--- provider.tf
+++ provider.tf
@@ -1,19 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
